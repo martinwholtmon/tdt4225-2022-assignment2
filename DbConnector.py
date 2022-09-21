92fd90cff4951f21c067f18acf953f4218d98878
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-09-21 20:19:38 +0200

    Reverted back the .env.template and casted the values as string instead

--- DbConnector.py
+++ DbConnector.py
@@ -1,3 +1,4 @@
+from ctypes import cast
 import mysql.connector as mysql
 from decouple import config
 
@@ -15,14 +16,18 @@ class DbConnector:
     PASSWORD = "test123" // The password you set for said user
     """
 
-    def __init__(self,
-                 HOST=config('HOST'),
-                 DATABASE=config('DATABASE'),
-                 USER=config('USER'),
-                 PASSWORD=config('PASSWORD')):
+    def __init__(
+        self,
+        HOST=config("HOST", cast=str),
+        DATABASE=config("DATABASE", cast=str),
+        USER=config("USER", cast=str),
+        PASSWORD=config("PASSWORD", cast=str),
+    ):
         # Connect to the database
         try:
-            self.db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=3306)
+            self.db_connection = mysql.connect(
+                host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=3306
+            )
         except Exception as e:
             print("ERROR: Failed to connect to db:", e)
 
